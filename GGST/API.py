e938f4181e454587f7a5ad2c70fd5c8408423dd9
Jérémy <jeremyschneider21@gmail.com>
2022-06-14 16:05:00 +0200

    Add method for building messagepack list

--- GGST/API.py
+++ GGST/API.py
@@ -1,10 +1,11 @@
+from optparse import OptionParser
 import msgpack
 import requests
 import logging
 import json
 import time
 
-from typing import Dict, List, Optional, Union, Any
+from typing import Dict, Optional, Union, Any
 from .constants import PLAYSTATION, PC, VERSION, CHARACTERS
 
 
@@ -32,7 +33,7 @@ class API:
 
     # endregion
 
-    def _request(self, endpoint: str, body: List) -> Union[List, None]:
+    def _request(self, endpoint: str, messagePack) -> Union[list, None]:
 
         headers = {
             "User-Agent": "Steam",
@@ -40,25 +41,38 @@ class API:
             "Cache-Control": "no-cache",
         }
         url: str = self.host + endpoint
-        messagePackData = msgpack.packb(body).hex()
 
         try:
-            res = requests.post(url, data={"data": messagePackData}, headers=headers)
+            res = requests.post(url, data={"data": messagePack}, headers=headers)
         except ConnectionError as e:
             logging.exception(f"An error as occured: {e}")
             return None
 
-        unpackedRes: List = msgpack.unpackb(res.content)
+        unpackedRes: list = msgpack.unpackb(res.content)
 
         return unpackedRes
 
-    def login(self, accountID: str, accountIDHex: str, platform: str) -> Union[List, None]:
+    def _msgpacking(self, platform: int, body: list, playerID: str = ""):
+        req: list = []
+
+        header: list = [
+            self.currentUser if self.currentUser != None else playerID,
+            self.token if self.token != None else "",
+            6,
+            VERSION,
+            self.platform if self.platform != None else platform,
+        ]
+
+        req.append(header)
+        req.append(body)
+        messagePack = msgpack.packb(req).hex()
+
+        return messagePack
+
+    def login(self, accountID: str, accountIDHex: str, platform: str) -> Union[list, None]:
         platformID: int = self._get_platform(platform)
 
-        data: List = [
-            ["", "", 6, VERSION, platformID],
-            [1, accountID, accountIDHex, 256, ""],
-        ]
+        data = self._msgpacking(platformID, [1, accountID, accountIDHex, 256, ""])
 
         res: Any = self._request("/api/user/login", data)
         self.token = res[0][0]
@@ -70,16 +84,7 @@ class API:
     def get_rcode(self, playerID: str, platform: str = "pc") -> Dict:
         platformID: int = self._get_platform(platform)
 
-        data: List = [
-            [
-                self.currentUser if self.currentUser != None else "",
-                self.token if self.token != None else "",
-                6,
-                VERSION,
-                self.platform if self.platform != None else platformID,
-            ],
-            [playerID, 7, -1, -1, -1, -1],
-        ]
+        data = self._msgpacking(platformID, [playerID, 7, -1, -1, -1, -1])
 
         res: Any = self._request("/api/statistics/get", data)
 
@@ -89,16 +94,7 @@ class API:
         platformID: int = self._get_platform(platform)
         characterID: int = self._get_character(character)
 
-        data: List = [
-            [
-                self.currentUser if self.currentUser != None else "",
-                self.token if self.token != None else "",
-                6,
-                VERSION,
-                self.platform if self.platform != None else platformID,
-            ],
-            [playerID, 1, 1, characterID, -1, -1],  # Character ID
-        ]
+        data = self._msgpacking(platformID, [playerID, 1, 1, characterID, -1, -1])
 
         res: Any = self._request("/api/statistics/get", data)
         return json.loads(res[1][1])
@@ -107,16 +103,7 @@ class API:
         platformID: int = self._get_platform(platform)
         characterID: int = self._get_character(character)
 
-        data: List = [
-            [
-                self.currentUser if self.currentUser != None else "",
-                self.token if self.token != None else "",
-                6,
-                VERSION,
-                self.platform if self.platform != None else platformID,
-            ],
-            [playerID, 2, 1, characterID, -1, -1],
-        ]
+        data = self._msgpacking(platformID, [playerID, 2, 1, characterID, -1, -1])
 
         res: Any = self._request("/api/statistics/get", data)
         return json.loads(res[1][1])
@@ -124,16 +111,7 @@ class API:
     def get_chara_level_ranking(self, playerID: str, page: int = 0, platform: str = "pc"):
         platformID: int = self._get_platform(platform)
 
-        data: List = [
-            [
-                self.currentUser if self.currentUser != None else playerID,
-                self.token if self.token != None else "",
-                6,
-                VERSION,
-                self.platform if self.platform != None else platformID,
-            ],
-            [page, 0, -1, 0],
-        ]
+        data = self._msgpacking(platformID, [page, 0, -1, 0], playerID)
 
         res: Any = self._request("/api/ranking/chara_level", data)
         return res[1][4]
@@ -141,16 +119,7 @@ class API:
     def get_vip_ranking(self, playerID: str, page: int = 0, platform: str = "pc"):
         platformID: int = self._get_platform(platform)
 
-        data: List = [
-            [
-                self.currentUser if self.currentUser != None else playerID,
-                self.token if self.token != None else "",
-                6,
-                VERSION,
-                self.platform if self.platform != None else platformID,
-            ],
-            [page, 0, -1, 0],
-        ]
+        data = self._msgpacking(platformID, [page, 0, -1, 0], playerID)
 
         res: Any = self._request("/api/ranking/vip", data)
         return res[1][4]
@@ -158,16 +127,7 @@ class API:
     def get_total_wins_ranking(self, playerID: str, page: int = 0, platform: str = "pc"):
         platformID: int = self._get_platform(platform)
 
-        data: List = [
-            [
-                self.currentUser if self.currentUser != None else playerID,
-                self.token if self.token != None else "",
-                6,
-                VERSION,
-                self.platform if self.platform != None else platformID,
-            ],
-            [page, 0, -1, 0],
-        ]
+        data = self._msgpacking(platformID, [page, 0, -1, 0], playerID)
 
         res: Any = self._request("/api/ranking/total_wins", data)
         return res[1][4]
@@ -175,16 +135,7 @@ class API:
     def get_survival_ranking(self, playerID: str, page: int = 0, platform: str = "pc"):
         platformID: int = self._get_platform(platform)
 
-        data: List = [
-            [
-                self.currentUser if self.currentUser != None else playerID,
-                self.token if self.token != None else "",
-                6,
-                VERSION,
-                self.platform if self.platform != None else platformID,
-            ],
-            [page, 0, -1, 0],
-        ]
+        data = self._msgpacking(platformID, [page, 0, -1, 0], playerID)
 
         res: Any = self._request("/api/ranking/survival", data)
         return res[1][4]
@@ -193,16 +144,7 @@ class API:
         platformID: int = self._get_platform(platform)
         currentMonth: str = time.strftime("%Y%m")
 
-        data: List = [
-            [
-                self.currentUser if self.currentUser != None else playerID,
-                self.token if self.token != None else "",
-                6,
-                VERSION,
-                self.platform if self.platform != None else platformID,
-            ],
-            [currentMonth, page, 0, -1, 0],
-        ]
+        data = self._msgpacking(platformID, [currentMonth, page, 0, -1, 0], playerID)
 
         res: Any = self._request("/api/ranking/monthly_wins", data)
         return res[1][4]
